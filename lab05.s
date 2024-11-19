fa23ee33bf3c038cb49243fc5a00ce64710d371c
helikot <helikotopoulos@yahoo.com>
2024-11-19 20:55:18 +0200

    change

--- lab05.s
+++ lab05.s
@@ -1,3 +1,4 @@
+# Once again i walk this lonely road, {this IS a cry for help}
 # ----------------------------------------------------------------------------------------
 # lab05.s 
 #  Verifies the correctness of some aspects of a 5-stage pipelined RISC-V implementation
